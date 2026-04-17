9963119bb9074a1b2712884223253a2431a600f1
Douglas Leandro <douglas.developer1000@gmail.com>
2026-04-17 10:46:51 -0300

    ➕ build: Inserting vite.config dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,31 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.ts',
+                'resources/js/bootstrap.ts',
+                'resources/js/components/packs/success-toast.ts',
+
+                'resources/css/pages/signup.css',
+                'resources/css/pages/signin.css',
+                'resources/css/pages/register-order.css',
+                'resources/css/pages/settings/user/edit.css',
+                'resources/css/pages/generic/create.css',
+                'resources/css/pages/generic/default.css',
+                'resources/css/pages/generic/index.css',
+                'resources/css/pages/generic/password.css',
+                'resources/css/pages/generic/table.css',
+
+                'resources/css/components/atoms/create-button.css',
+                'resources/css/components/atoms/gate/card.css',
+                'resources/css/components/atoms/gate/container.css',
+                'resources/css/components/atoms/gate/heading.css',
+                'resources/css/components/atoms/gate/logo.css',
+                'resources/css/components/packs/gate-back-img.css',
+                'resources/css/components/packs/header.css',
+                'resources/css/components/packs/page-heading-row.css',
+            ],
             refresh: true,
         }),
     ],
