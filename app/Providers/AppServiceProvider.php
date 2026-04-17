4f38ca5f97b7e1d3806ac5bb612bbdcc2fc4ba6a
Douglas Leandro <douglas.developer1000@gmail.com>
2026-04-17 14:21:38 -0300

    🧹 cleanup: Removing useless imports

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
