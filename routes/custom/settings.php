bb90335b5b24a4d8fffa740133829362bceb8c2d
Douglas Leandro <douglas.developer1000@gmail.com>
2026-04-24 23:54:37 -0300

    :wrench: fix: Adjusting settings access permission

--- routes/custom/settings.php
+++ routes/custom/settings.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SettingsUserController;
-use App\Libraries\Enums\RoleNameEnum;
+use App\Libraries\Enums\PermissionNameEnum;
 
-Route::middleware(['role:' . RoleNameEnum::USER->value])->group(function () {
+Route::middleware(['can:' . PermissionNameEnum::HEADER_SETTINGS->value])->group(function () {
     Route::get('/user', [SettingsUserController::class, 'show'])->name('settings.user.show');
     Route::get('/user/{user}', [SettingsUserController::class, 'edit'])->name('settings.user.edit');
     Route::put('/user/{user}', [SettingsUserController::class, 'update'])->name('settings.user.update');
