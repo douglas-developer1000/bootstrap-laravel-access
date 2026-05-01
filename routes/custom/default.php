891afa66ef45690cca1a8c6944de82930fa86f30
Douglas Leandro <douglas.developer1000@gmail.com>
2026-05-01 20:01:08 -0300

    🔧 refactor: Applying auth middleware in images' requests

--- routes/custom/default.php
+++ routes/custom/default.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/signout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 Route::view('/dashboard', 'pages.dashboard')->name('dashboard')->middleware(['auth', 'verified']);
-Route::get('/storage/app/{folder}/{filename}', [ImageController::class, 'find'])->name('user.photo.show');
+Route::get('/storage/app/{folder}/{filename}', [ImageController::class, 'find'])->name('user.photo.show')->middleware('auth');
