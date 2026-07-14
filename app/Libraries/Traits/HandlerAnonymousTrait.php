a828f9e9473bc92f067362e479a4b4500df10c58
Douglas Leandro <douglas.developer1000@gmail.com>
2026-07-14 15:03:24 -0300

    🔧 refactor: Centralizing the anonymous name

--- app/Libraries/Traits/HandlerAnonymousTrait.php
+++ app/Libraries/Traits/HandlerAnonymousTrait.php
@@ -22,19 +22,28 @@ trait HandlerAnonymousTrait
         )->first()->users();
     }
 
+    /**
+     * NOTE: If there is a language requirement, this method must search the "anonymous name"
+     * by user locale
+     *
+     * @return string The anonymous name used by automatic and hidden tagging context
+     */
+    public static function getAnonymousName(): string
+    {
+        return 'Anônimo';
+    }
+
     public static function getAnonymous(): self
     {
         return static::firstOrCreate([
             'native' => true,
-            'name' => 'anonymous',
+            'name' => self::getAnonymousName(),
             'user_id' => self::getSuperAdmins()->first(['id'])->id,
         ]);
     }
 
     public function scopeNotAnonymous(Builder $query): Builder
     {
-        return $query
-            ->where('native', true)
-            ->whereNot('name', 'anonymous');
+        return $query->whereNot('name', self::getAnonymousName());
     }
 }
