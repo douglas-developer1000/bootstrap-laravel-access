30b7e7571f5e6f62c32fd8356e36581277161d4b
Douglas Leandro <douglas.developer1000@gmail.com>
2026-07-13 21:19:48 -0300

    🧹 cleanup: Comments

--- database/migrations/2026_05_06_141830_create_sale_stock_exit_table.php
+++ database/migrations/2026_05_06_141830_create_sale_stock_exit_table.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-/**
- * @see App\Models\Payment::class
- */
 return new class extends Migration
 {
     /**
