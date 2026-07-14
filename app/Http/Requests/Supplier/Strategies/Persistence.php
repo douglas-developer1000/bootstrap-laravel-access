c255436f52ee30136a9502f5cb28206e46f1f8a6
Douglas Leandro <douglas.developer1000@gmail.com>
2026-07-14 15:04:21 -0300

    🔧 refactor: Removing useless class

--- app/Http/Requests/Supplier/Strategies/Persistence.php
+++ app/Http/Requests/Supplier/Strategies/Persistence.php
@@ -10,24 +10,35 @@ use App\Libraries\Traits\ImgCheckerTrait;
 use App\Libraries\Traits\NativeCheckerTrait;
 use App\Libraries\Traits\OneOrManyMsgTrait;
 use App\Libraries\Values\CnpjValue;
+use App\Models\Supplier;
+use App\Models\User;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 
-class Persistence implements Checker
+final class Persistence implements Checker
 {
-    use OneOrManyMsgTrait, NativeCheckerTrait, ImgCheckerTrait;
+    use ImgCheckerTrait, NativeCheckerTrait, OneOrManyMsgTrait;
+
     protected bool $isSuperAdmin;
+
     protected int|string $userId;
+
     protected int $nameMinSize;
+
     protected int $nameMaxSize;
+
     protected int $obsMaxSize;
 
-    public function __construct(protected FormRequest $formRequest, protected bool $imgRequired = TRUE)
-    {
+    public function __construct(
+        protected FormRequest $formRequest,
+        protected bool $imgRequired = true,
+        protected ?Supplier $supplier = null
+    ) {
         /**
-         * @var \App\Models\User $user
+         * @var User $user
          */
         $user = Auth::user();
         $this->isSuperAdmin = $user->hasRole('super-admin');
@@ -50,11 +61,12 @@ class Persistence implements Checker
         if ($this->isSuperAdmin) {
             return $this->pickImgRules(required: $this->imgRequired);
         }
+
         return [
             'color' => [
                 'required',
                 Rule::enum(SupplierColorEnum::class),
-            ]
+            ],
         ];
     }
 
@@ -63,6 +75,7 @@ class Persistence implements Checker
         if ($this->isSuperAdmin) {
             return $this->pickImgMessages(required: $this->imgRequired);
         }
+
         return [
             'color.required' => 'Selecione um cor válida',
             'color.in' => 'Selecione um cor válida',
@@ -76,19 +89,25 @@ class Persistence implements Checker
                 'required',
                 "min:{$this->nameMinSize}",
                 "max:{$this->nameMaxSize}",
-                Rule::unique('suppliers', 'name')->where(function (Builder $query) {
-                    $query->where([
-                        'user_id' => $this->userId
-                    ])->orWhere([
-                        'native' => 1
-                    ]);
-                })
+                Rule::unique('suppliers', 'name')->where(fn (Builder $query) => (
+                    $query
+                        ->where('user_id', $this->userId)
+                        ->orWhere('native', 1)
+                        ->when(
+                            $this->supplier,
+                            fn (Builder $query, Supplier $supplier) => $query->ignore(
+                                $supplier->id,
+                                'id'
+                            )
+                        )
+                )),
+                Str::of('different:')->append(Supplier::getAnonymousName())->toString(),
             ],
             'cnpj' => [
                 'bail',
                 'nullable',
                 CnpjValue::rule(),
-                CnpjValue::uniqueRule('suppliers'),
+                CnpjValue::uniqueRule('suppliers', $this->supplier?->id),
             ],
             'obs' => [
                 'nullable',
@@ -104,9 +123,9 @@ class Persistence implements Checker
         $msgs = collect([
             'nameMinMsg' => $this->nameMinSize,
             'nameMaxMsg' => $this->nameMaxSize,
-            'obsMaxMsg' => $this->obsMaxSize
+            'obsMaxMsg' => $this->obsMaxSize,
         ])->map(
-            fn(int $qty) => $this->makeSizeMsg($qty, 'caracter', 'caracteres')
+            fn (int $qty) => $this->makeSizeMsg($qty, 'caracter', 'caracteres')
         )->all();
 
         return [
@@ -114,6 +133,7 @@ class Persistence implements Checker
             'name.min' => "Tamanho mínimo: {$msgs['nameMinMsg']}",
             'name.max' => "Tamanho máximo excedido: {$msgs['nameMaxMsg']}",
             'name.unique' => 'Nome já utilizado',
+            'name.different' => 'Valor reservado. Por favor escolha outro.',
 
             'cnpj.unique' => 'Cnpj já utilizado!',
 
--- app/Http/Requests/Supplier/SupplierRequest.php
+++ app/Http/Requests/Supplier/SupplierRequest.php
@@ -6,15 +6,13 @@ namespace App\Http\Requests\Supplier;
 
 use App\Http\Requests\Checker;
 use App\Http\Requests\CustomFormRequest;
-use App\Http\Requests\Supplier\Strategies\Persistence;
-use App\Http\Requests\Supplier\Strategies\Update;
 use App\Http\Requests\Supplier\Strategies\DestroyGroup;
+use App\Http\Requests\Supplier\Strategies\Persistence;
 use App\Http\Requests\Supplier\Strategies\RestoreGroup;
 use Exception;
 
 final class SupplierRequest extends CustomFormRequest
 {
-
     protected function pickChecker(): Checker
     {
         $url = url()->current();
@@ -22,19 +20,19 @@ final class SupplierRequest extends CustomFormRequest
             case route('suppliers.store'):
                 return new Persistence($this);
             case route('suppliers.update', $this->route('supplier', 0)):
-                return new Update($this, $this->route('supplier'));
+                return new Persistence($this, false, $this->route('supplier'));
             case route('suppliers.group.destroy', [
                 'key' => $this->route('key', 'key'),
-                'supplierList' => 'list'
+                'supplierList' => 'list',
             ]):
                 return new DestroyGroup();
             case route('suppliers.group.restore', [
                 'key' => $this->route('key', 'key'),
-                'supplierList' => 'trashed'
+                'supplierList' => 'trashed',
             ]):
                 return new RestoreGroup();
             default:
-                throw new Exception("Method Not Implemented", 1);
+                throw new Exception('Method Not Implemented', 1);
         }
     }
 
