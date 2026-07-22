c6d78874c379973307785988b71f8697b7232629
Douglas Leandro <douglas.developer1000@gmail.com>
2026-07-22 15:45:59 -0300

    🐛 fix: Fixing usleep params

--- app/Libraries/Helpers/TimeProtection.php
+++ app/Libraries/Helpers/TimeProtection.php
@@ -7,11 +7,25 @@ namespace App\Libraries\Helpers;
 final class TimeProtection
 {
     /**
+     * Because the usleep native method only allows int values
+     * and the microseconds' calculation results can be floats
+     * then it's necessary convert them to int.
+     */
+    protected static function runSleep(float|int $microseconds): void
+    {
+        \usleep(
+            \intval(
+                \round($microseconds * 1000)
+            )
+        );
+    }
+
+    /**
      * Compare strings on secure way vs timing attacks
      */
     public static function secureCompare(string $known, string $input): bool
     {
-        return hash_equals($known, $input);
+        return \hash_equals($known, $input);
     }
 
     /**
@@ -19,27 +33,28 @@ final class TimeProtection
      */
     public static function execWithProtection(callable $callback, int $minExecutionTime = 500): mixed
     {
-        $startTime = microtime(true);
+        $startTime = \microtime(true);
         $result = $callback();
-        $elapsedTime = (microtime(true) - $startTime) * 1000;
+        $elapsedTime = (\microtime(true) - $startTime) * 1000;
 
         if ($elapsedTime < $minExecutionTime) {
-            $additionalDelay = ($minExecutionTime - $elapsedTime) + random_int(0, 200);
-            usleep($additionalDelay * 1000);
+            $additionalDelay = ($minExecutionTime - $elapsedTime) + \random_int(0, 200);
+            self::runSleep($additionalDelay);
         }
+
         return $result;
     }
 
     public static function gaussianDelay(float $mean = 300, float $stdDev = 100): void
     {
-        $u1 = random_int(1, 10000) / 10000;
-        $u2 = random_int(1, 10000) / 10000;
+        $u1 = \random_int(1, 10000) / 10000;
+        $u2 = \random_int(1, 10000) / 10000;
 
         // Box-Muller transform
-        $randStdNormal = sqrt(-2 * log($u1)) * sin(2 * M_PI * $u2);
-        $delay = max(0, $mean + $stdDev * $randStdNormal);
+        $randStdNormal = \sqrt(-2 * \log($u1)) * \sin(2 * M_PI * $u2);
+        $delay = \max(0, $mean + $stdDev * $randStdNormal);
 
-        usleep((int)$delay * 1000);
+        self::runSleep($delay);
     }
 
     /**
@@ -48,7 +63,7 @@ final class TimeProtection
      */
     public function fakePasswordVerification(string $password): void
     {
-        bcrypt($password);
+        \bcrypt($password);
 
         self::gaussianDelay(100, 50);
     }
